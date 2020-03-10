4f5c3660a380bd607eff8911792086b3ed7b75a7
zhaoyouwen <zhaoyouwen@dgg.net>
2020-03-10 10:20:12 +0800

    1

---
+++ element/src/components/common/Timer.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>{{this.time}}</span>
+</template>
+
+<script>
+    export default {
+        name: "Timer",
+        props:{time: {type: Number, required: true,default:60}},
+        data() {
+            return {timer: null}
+        },
+        methods: {
+            countDown() {
+                this.time--;
+                if (this.time === 0 && this.timer !== null) {
+                    window.clearInterval(timer);
+                }
+            },
+            run() {
+                this.timer = setInterval(this.countDown, 1000)
+            }
+        },
+        mounted() {
+            this.run();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- element/src/main.js
+++ element/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import el from 'element-ui'
-import login from './components/system/Login'
-import index from './components/system/Index'
+import timer from './components/common/Timer'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
@@ -11,13 +10,10 @@ Vue.use(el);
 new Vue({
   el: '#app',
   data() {
-    return {
-      img1: 'https://testdggoss1.oss-cn-beijing.aliyuncs.com/86feca9b-cc55-44e9-8381-857559dc75e1.jpg',
-      img2: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558779551&di=0db67f52a2d664fb17ff1d2ea741e3bc&imgtype=jpg&er=1&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fdaae29cad3d6a36f5619a692f0450f0501c76a4e.png'
-    };
+    return {};
   },
-  components: {login, index},
-  template: '<index />'
+  components: {timer},
+  template: '<timer :time="60"/>'
 })
 
 
